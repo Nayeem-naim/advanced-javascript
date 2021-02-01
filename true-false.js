d63c88a841e3ca38e141c4ff30665e47e6f3def8
Nayeem-naim <mdnaimislam405@gmail.com>
2021-02-01 23:17:35 +0600

    true-false

---
+++ true-false.js
@@ -0,0 +1,24 @@
+// falsy
+// 0
+// "" faka   string  false //( " " )space string is true
+// undefine 
+// null 
+// Nan 
+// false
+
+// truthy
+
+// "0"
+// " "
+// []
+//  {}
+// 'false'
+
+const name = "0";
+
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
